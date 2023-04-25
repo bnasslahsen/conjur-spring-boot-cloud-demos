0588507051776be3092940a5b6e6958bd32fbaa3
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-25 23:23:43 +0200

    Samples update

--- demo-service/src/main/java/com/cyberark/conjur/plugintest/ConjurSpringCloudPluginTest.java
+++ demo-service/src/main/java/com/cyberark/conjur/plugintest/ConjurSpringCloudPluginTest.java
@@ -1,14 +1,12 @@
 package com.cyberark.conjur.plugintest;
 
-import com.cyberark.conjur.springboot.annotations.ConjurValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
@@ -16,15 +14,17 @@ public class ConjurSpringCloudPluginTest implements CommandLineRunner{
 	private static Logger logger = LoggerFactory.getLogger(ConjurSpringCloudPluginTest.class);
 
 
-	@ConjurValue(key="data/bnl/ocp-apps/url")
+	@Value("${data/bnl/ocp-apps/url}")
+	//@ConjurValue(key="data/bnl/ocp-apps/url")
 	private byte[] url;
-
-	/**
-	 * The Username.
-	 */
-	@ConjurValue(key ="data/bnl/ocp-apps/username")
-	private byte[] username;
 	
+	@Value("${data/bnl/ocp-apps/username}")
+	//@ConjurValue(key ="data/bnl/ocp-apps/username")
+	private byte[] username;
+
+
+	@Value("${data/bnl/ocp-apps/password}")
+	private byte[] password;
 	
     public static void main(String[] args) {
         SpringApplication.run(ConjurSpringCloudPluginTest.class, args);
@@ -32,8 +32,9 @@ public class ConjurSpringCloudPluginTest implements CommandLineRunner{
 
     
 	public void run(String... args) {
-		logger.info("By Using Standard Spring annotation -->  " + new String(url) + "  " );
 		logger.info("By Using Standard Spring annotation -->  " + new String(username) + "  " );
+		logger.info("By Using Standard Spring annotation -->  " + new String(url) + "  " );
+		logger.info("By Using Standard Spring annotation -->  " + new String(password) + "  " );
 	}
 
 	
